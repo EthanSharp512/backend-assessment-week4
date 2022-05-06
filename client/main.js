5254d7d4fe4e3307940a2e3cd189a5cc91048079
Ethan Sharp <ethansharpdev@gmail.com>
2022-05-06 13:12:14 -0600

    progress on post function

--- client/main.js
+++ client/main.js
@@ -1,14 +1,5 @@
 const complimentBtn = document.getElementById("complimentButton")
 const fortuneBtn = document.getElementById("fortuneButton")
-const postFortunesBtn = document.getElementById("postFortunesButton")
-
-
-
-
-
-
-
-
 
 
 const getCompliment = () => {
@@ -29,32 +20,64 @@ const getCompliment = () => {
     };
     
 
-    const createCompliment = body => 
-        axios.post("/api/createCompliment", body)
-        .then(res = > {
-            const data = res.data;
-            alert(data);
-        };
+
+baseURL = "/api/createCompliment"
+
+
+const complimentsCallback = ({ data: compliments }) => displayCompliments(compliments)
+const errCallback = err => console.log(err)
+
+const getAllCompliments = () => axios.get(baseURL).then(complimentsCallback).catch(errCallback)
+const createCompliment = body => axios.post(baseURL, body).then(complimentsCallback).catch(errCallback)
+// const deleteCompliment = id => axios.delete(`${baseURL}/${id}`).then(complimentsCallback).catch(errCallback)
+// const updateCompliment = (id, type) => axios.put(`${baseURL}/${id}`, {type}).then(complimentsCallback).catch(errCallback)
     
     
-    function submitHandler(e) {
-        e.preventDefault()
+function submitHandler(e) {
+    e.preventDefault()
     
-        let compliment = document.querySelector('#compliment')
-        let type = document.querySelector('#type')
+    let compliment = document.querySelector('#compliment')
+    let type = document.querySelector('#type')
     
-        let bodyObj = {
-            compliment: compliment.value,
-            type: type.value
-        }
-    
-        createCompliment(bodyObj)
-    
-        compliment.value = ''
-        type.value = ''
+    let bodyObj = {
+        compliment: compliment.value,
+        type: type.value
     }
+    
+    createCompliment(bodyObj)
+    
+    compliment.value = ''
+    type.value = ''
+}
+
+
+
+function createComplimentCard(yourCompliment) {
+    const complimentCard = document.createElement('div')
+    complimentCard.classList.add('compliment-card')
+
+    complimentCard.innerHTML = `<p class="compliment">${yourCompliment.compliment}</p>`
+
+    complimentsContainer.appendChild(complimentCard)
+}
 
     
-    complimentBtn.addEventListener('click', getCompliment)
-    fortuneBtn.addEventListener('click', getFortune)
-    postFortunesBtn.addEventListener('click', postFortunes)
+
+function displayCompliments(arr) {
+    complimentsContainer.innerHTML = ``
+    for (let i = 0; i < arr.length; i++) {
+        createComplimentCard(arr[i])
+    }
+}
+
+form.addEventListener('submit', submitHandler)
+
+getAllCompliments()
+
+
+
+
+
+complimentBtn.addEventListener('click', getCompliment)
+fortuneBtn.addEventListener('click', getFortune)
+postFortunesBtn.addEventListener('click', postFortunes)
